415b1b352c2b954b09111f5c689c98d38c81058b
Rui Batista <ruiandrebatista@gmail.com>
2017-04-01 13:11:09 +0100

    Bump dependency versions-

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,8 +5,8 @@ object Dependencies {
   lazy val specs2Core = "org.specs2" %% "specs2-core" % specs2Version
   lazy val specs2Scalacheck = "org.specs2" %% "specs2-scalacheck" % specs2Version
 
-  val fs2Version = "0.9.3"
-  val specs2Version = "3.8.8"
+  val fs2Version = "0.9.4"
+  val specs2Version = "3.8.9"
 }
 
 
